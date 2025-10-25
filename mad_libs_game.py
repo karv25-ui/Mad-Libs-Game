54e2ea75844835eb0cc91e0bf31ffbfc8c8c0e94
karv25-ui <karvenda@gmail.com>
2025-10-25 12:56:32 -0400

    Fix syntax errors in mad_libs_game.py

--- mad_libs_game.py
+++ mad_libs_game.py
@@ -1,3 +1,4 @@
 # This is the section where you add your sentences with blanks!
-
+noun_1
 print("Today is " noun_1 " and it is a great day to be " verb_1)
+print("This " noun_2 " is great " noun_2 " to be in, especially this time around.")
